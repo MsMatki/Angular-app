4762e7065283eae272244d3ab36efa1dd52ff6ac
Bartol Bilankov <bartol.bilankov@gmail.com>
2019-01-02 00:02:23 -0500

    install Bootstrap v4 + navbar component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,57 +9,51 @@ import {User} from '../../models/User'
 export class UsersComponent implements OnInit {
   users: User[];
   showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    
-    this.users = [
-     /* {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address:{
-            street: '50 Main St',
-            city: 'New York',
-            state: 'US'
-        }
-    },
+
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address:{
+              street: '50 Main St',
+              city: 'New York',
+              state: 'US'
+          }
+      },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 52,
+          address:{
+              street: '20 School Street',
+              city: 'Chicago',
+              state: 'US'
+          }
+      },
       {
-        firstName: 'Kevin',
-        lastName: 'Johnson',
-        age: 52,
+        firstName: 'Bartol',
+        lastName: 'Bilankov',
+        age: 27,
         address:{
-            street: '20 School Street',
-            city: 'Chicago',
-            state: 'US'
-        }
-    },
-    {
-      firstName: 'Bartol',
-      lastName: 'Bilankov',
-      age: 27,
-      address:{
-          street: '40 Henry Welsh Dr',
-          city: 'Toronto',
-          state: 'Canada'
-      }
-  }*/
-    ];
-
-    this.showExtended = false;
-
-   /* this.addUser({
-        firstName: 'David',
-        lastName: 'Jackson',
-        age: 20,
-        address:{
-            street: '12 Martin Square',
-            city: 'London',
-            state: 'England'
+            street: '40 Henry Welsh Dr',
+            city: 'Toronto',
+            state: 'Canada'
         }
     }
-    );*/
+      ];
+      this.loaded = true;
+
+    this.addUser({
+        firstName: 'David',
+        lastName: 'Jackson',
+    }
+    );
   }
 
   addUser(user: User){
