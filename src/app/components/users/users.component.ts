fe7fbf74bf64724db086eb7a3f85b23e505a7381
Bartol Bilankov <bartol.bilankov@gmail.com>
2019-01-13 21:28:54 -0500

    update v6

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -42,7 +42,7 @@ export class UsersComponent implements OnInit {
       value.isActive = true;
       value.registered = new Date();
       value.hide = true;
-      this.users.unshift(value);
+      this.dataService.addUser(value);
 
       this.form.reset();
     }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -41,4 +41,9 @@ export class DataService {
      console.log('fetching users from service')
      return this.users;
    }
+
+
+   addUser(user: User){
+    this.users.unshift(user);
+   }
 }
